ac4b750c4032d0589c098feeb6b0e2fbb43b4daa
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-02-23 21:00:29 +0100

    Add debug_log support
    Signed-off-by: Andy Lo-A-Foe <andy.loafoe@gmail.com>

--- ssh/config.go
+++ ssh/config.go
@@ -2,11 +2,18 @@ package ssh
 
 import (
 	"fmt"
+	"os"
 )
 
 type Config struct {
+	DebugLog  string
+	debugFile *os.File
 }
 
 func (c *Config) Debug(format string, args ...interface{}) (int, error) {
+	if c.debugFile != nil {
+		output := fmt.Sprintf(format, args...)
+		return c.debugFile.WriteString(output)
+	}
 	return fmt.Printf(format, args...)
 }
--- ssh/provider.go
+++ ssh/provider.go
@@ -2,6 +2,7 @@ package ssh
 
 import (
 	"context"
+	"os"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -10,7 +11,13 @@ import (
 // Provider
 func Provider() *schema.Provider {
 	return &schema.Provider{
-		Schema: map[string]*schema.Schema{},
+		Schema: map[string]*schema.Schema{
+			"debug_log": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "File to write debugging info to",
+			},
+		},
 		ResourcesMap: map[string]*schema.Resource{
 			"ssh_resource": resourceResource(),
 		},
@@ -23,5 +30,17 @@ func providerConfigure(_ context.Context, d *schema.ResourceData) (interface{}, 
 	config := &Config{}
 
 	var diags diag.Diagnostics
+
+	config.DebugLog = d.Get("debug_log").(string)
+
+	if config.DebugLog != "" {
+		debugFile, err := os.OpenFile(config.DebugLog, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+		if err != nil {
+			config.debugFile = nil
+		} else {
+			config.debugFile = debugFile
+		}
+	}
+
 	return config, diags
 }
