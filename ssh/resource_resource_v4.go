761eb4497e75cbf62ca8dbe97779c504c70a75dd
Sergey Kvartnikov <snkvartnik@mts.ru>
2024-01-23 11:02:32 +0300

    Feature: added error ignoring by option

---
+++ ssh/resource_resource_v4.go
@@ -0,0 +1,151 @@
+package ssh
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+func patchResourceV4(_ context.Context, rawState map[string]interface{}, _ interface{}) (map[string]interface{}, error) {
+	if rawState == nil {
+		rawState = map[string]interface{}{}
+	}
+	rawState["ignore_no_supported_methods_remain"] = false
+	return rawState, nil
+}
+
+func resourceResourceV4() *schema.Resource {
+	return &schema.Resource{
+		SchemaVersion: 2,
+		Schema: map[string]*schema.Schema{
+			"when": {
+				Description:  "Determines when the commands is to be executed. Options are 'create' or 'destroy'",
+				Type:         schema.TypeString,
+				Optional:     true,
+				Default:      "create",
+				ValidateFunc: validation.StringInSlice([]string{"create", "destroy"}, false),
+			},
+			"triggers": {
+				Description: "A map of arbitrary strings that, when changed, will force the 'hsdp_container_host_exec' resource to be replaced, re-running any associated commands.",
+				Type:        schema.TypeMap,
+				Optional:    true,
+				ForceNew:    true,
+			},
+			"host": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"port": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "22",
+			},
+			"bastion_host": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"bastion_port": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "22",
+			},
+			"user": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"host_user": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+			"private_key": {
+				Type:      schema.TypeString,
+				Optional:  true,
+				Sensitive: true,
+			},
+			"host_private_key": {
+				Type:      schema.TypeString,
+				Optional:  true,
+				Sensitive: true,
+			},
+			"agent": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"pre_commands": {
+				Type:     schema.TypeList,
+				MaxItems: 100,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"commands": {
+				Type:     schema.TypeList,
+				MaxItems: 100,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"commands_after_file_changes": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  true,
+			},
+			"timeout": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "5m",
+			},
+			"ignore_no_supported_methods_remain": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"retry_delay": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "10s",
+			},
+			"result": {
+				Type:      schema.TypeString,
+				Computed:  true,
+				Sensitive: false,
+			},
+			"file": {
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"source": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"content": {
+							Type:      schema.TypeString,
+							Optional:  true,
+							Sensitive: false,
+						},
+						"destination": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"permissions": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"owner": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"group": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
